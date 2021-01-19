62b9f0449f29fff45d97f47fbb19135c2623b53f
xiaoboya <xby@Gerrit123>
2021-01-19 10:07:45 +0800

    test

---
+++ common.go
@@ -0,0 +1,16 @@
+package minimin
+
+const (
+	Success = "SUCCESS" // 成功
+	Fail    = "FAIL"    // 失败
+	Run     = "RUNING"  // 运行中
+	Init    = "INIT"    // 初始化
+	Queue   = "QUEUE"   // 队列中
+
+	SimpleFilePerm = 0666
+	DangerPerm     = 0777
+
+	InfoDir = "info"
+
+	AppListFile = "apps.json"
+)
---
+++ path.go
@@ -0,0 +1,67 @@
+package minimin
+
+import (
+	"os"
+	"strings"
+)
+
+var (
+	PATH = ""
+)
+
+// InitStorage 初始化存储路径
+func InitStorage(path string) (err error) {
+	if PathNotExist(path) {
+		err = os.Mkdir(path, SimpleFilePerm)
+		if err == nil {
+			PATH = path
+		} else {
+			return err
+		}
+	} else {
+		PATH = path
+	}
+	return nil
+}
+
+// GetBasePath 获取当前的路径
+func GetBasePath() string {
+	switch PATH {
+	case "":
+		dir, _ := os.Getwd()
+		return dir
+	default:
+		return PATH
+	}
+}
+
+// PathJoin 路径拼接
+func PathJoin(path string, filename string) string {
+	filename = strings.Trim(filename, "/")
+	path = strings.TrimRight(path, "/")
+	return path + "/" + filename
+}
+
+// PathExist 路径存在
+func PathExist(path string) (res bool) {
+	var err error
+	_, err = os.Stat(path)
+	switch err {
+	case nil:
+		return true
+	default:
+		return false
+	}
+}
+
+// PathNotExist 路径不存在
+func PathNotExist(path string) (res bool) {
+	var err error
+	_, err = os.Stat(path)
+	switch err {
+	case os.ErrNotExist:
+		return true
+	default:
+		return false
+	}
+}
